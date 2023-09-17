6139fcaff24f7d51087e0c72607c8dd54203c362
nashdev255 <yutori1107@gmail.com>
2023-09-17 23:54:32 +0900

    add Monster

---
+++ Characters/Fighters/Fighter.js
@@ -0,0 +1,9 @@
+import Character from "../Character";
+
+class Fighter extends Character {
+  constructor(name, maxhp, atk, def, spd) {
+    super(name, maxhp, atk, def, spd);
+  }
+}
+
+export default Fighter;
