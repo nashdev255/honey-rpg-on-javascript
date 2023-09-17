8d077ae0015bb79db8c128f417548974779b6153
nashdev255 <yutori1107@gmail.com>
2023-09-17 23:55:42 +0900

    add Monster

---
+++ Characters/Monsters/Monster.js
@@ -0,0 +1,9 @@
+import Character from "../Character";
+
+class Monster extends Character {
+  constructor(name, maxhp, atk, def, spd) {
+    super(name, maxhp, atk, def, spd);
+  }
+}
+
+export default Monster;
